1e7fbcb8ec660521cd2daa658519162bc954a5a6
RisuSofos <risusofos@gmail.com>
2021-06-07 12:01:15 -0500

    [BUG] Fixed overlapping keybinds

--- config.h
+++ config.h
@@ -108,7 +108,7 @@ static Key keys[] = {
 	{ MODKEY|ControlMask,		XK_0,		togglegaps,		{0} },
 	{ MODKEY|ControlMask|ShiftMask,	XK_0,		defaultgaps,		{0} },
 	{ MODKEY,			XK_Tab,		view,			{0} },
-	{ MODKEY|ShiftMask,		XK_c,		killclient,		{0} },
+	{ MODKEY,			XK_q,		killclient,		{0} },
 	{ MODKEY,			XK_z,		setlayout,		{.v = &layouts[0]} },
 	{ MODKEY,			XK_x,   	setlayout,		{.v = &layouts[1]} },
 	{ MODKEY|ShiftMask,		XK_x,   	setlayout,		{.v = &layouts[2]} },
@@ -139,7 +139,7 @@ static Key keys[] = {
 	TAGKEYS(			XK_7,					6)
 	TAGKEYS(			XK_8,					7)
 	TAGKEYS(			XK_9,					8)
-	{ MODKEY|ShiftMask,		XK_q,		quit,			{0} },
+	{ MODKEY|ControlMask|ShiftMask,	XK_q,		quit,			{0} },
 };
 
 /* button definitions */
